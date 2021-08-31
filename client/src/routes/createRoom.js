a73b7c58ce069638d46137e552ad1bcb7b0592f3
sonalsk <sonalkushwaha039@gmail.com>
2021-08-31 22:05:35 +0530

    MLSA Event Code - Video app with JS

---
+++ client/src/routes/createRoom.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {v1 as uuid} from "uuid";
+
+const CreateRoom = (props) => {
+    function create() {
+        const id = uuid();
+        props.history.push(`/room/${id}`);
+    }
+    return (
+        <button onClick = {create}> Start Video Call </button>
+    );
+}
+
+export default CreateRoom;
